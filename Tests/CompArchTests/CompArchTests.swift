c526f89e7d1d1c825fda1b8606359e47013fd3eb
Sven A. Schmidt <sas@finestructure.co>
2020-02-16 11:21:28 +0100

    Add test for Identified matching

--- Tests/CompArchTests/CompArchTests.swift
+++ Tests/CompArchTests/CompArchTests.swift
@@ -5,7 +5,7 @@ import XCTest
 
 final class CompArchTests: XCTestCase {
     func test_matchingIndexed() {
-        let reducer: Reducer<Parent.State, Parent.Action> = { state, action in
+        let reducer: Reducer<IndexedParent.State, IndexedParent.Action> = { state, action in
             switch action {
                 case let .child((index, .childAction1)):
                     return [
@@ -18,8 +18,40 @@ final class CompArchTests: XCTestCase {
                     return []
             }
         }
-        var parent = Parent.State(parentProp: 0)
+        var parent = IndexedParent.State(parentProp: 0)
         let effects = reducer(&parent, .child(Indexed(1, .childAction1)))
+        XCTAssertEqual(effects.count, 1)
+
+        let exp = expectation(description: "exp")
+        let effect = effects[0]
+        let _ = effect.sink { action in
+            if case let .childAction(index) = action {
+                XCTAssertEqual(index, 1)
+            } else {
+                XCTFail("failed to extract index from childAction")
+            }
+            exp.fulfill()
+        }
+        waitForExpectations(timeout: 0.5)
+    }
+
+    func test_matchingIdentified() {
+        let reducer: Reducer<IdentifiedParent.State, IdentifiedParent.Action> = { state, action in
+            switch action {
+                case let .child((index, .childAction1)):
+                    return [
+                        Just(.childAction(index))
+                            .eraseToEffect()
+                ]
+                case .child(_):
+                    return []
+                case .childAction(_):
+                    return []
+            }
+        }
+        var parent = IdentifiedParent.State(parentProp: 0)
+        let action = Identified<Child.State, Child.Action>(id: 1, action: .childAction1)
+        let effects = reducer(&parent, .child(action))
         XCTAssertEqual(effects.count, 1)
 
         let exp = expectation(description: "exp")
@@ -37,7 +69,7 @@ final class CompArchTests: XCTestCase {
 }
 
 
-struct Parent {
+struct IndexedParent {
     struct State {
         var parentProp: Int
     }
@@ -49,8 +81,20 @@ struct Parent {
 }
 
 
-struct Child {
+struct IdentifiedParent {
     struct State {
+        var parentProp: Int
+    }
+
+    enum Action {
+        case child(Identified<Child.State, Child.Action>)
+        case childAction(Int)
+    }
+}
+
+
+struct Child {
+    struct State: Identifiable {
         var id: Int { childProp }
         var childProp: Int
     }
