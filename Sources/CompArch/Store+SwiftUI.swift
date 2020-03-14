d34222a97f9e6d9c0872b6580b7f08de3b219b7f
Sven A. Schmidt <sas@finestructure.co>
2020-03-14 10:13:06 +0100

    Add SwiftUI binding getter

---
+++ Sources/CompArch/Store+SwiftUI.swift
@@ -0,0 +1,22 @@
+//
+//  File.swift
+//  
+//
+//  Created by Sven A. Schmidt on 14/03/2020.
+//
+
+import CasePaths
+import SwiftUI
+
+
+extension Store {
+    // https://twitter.com/alexito4/status/1228373956777979905?s=21
+    func binding<T>(value: KeyPath<Value, T>,
+                    action: CasePath<Action, T>) -> Binding<T> {
+        Binding<T>(
+            get: { self.value[keyPath: value]  },
+            set: { self.send(action.embed($0)) }
+        )
+    }
+}
+
